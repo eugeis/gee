3ad0129320f720384c2735006a3fe400bdd233ac
Eugen Eisler <eoeisler@gmail.com>
2017-05-22 17:47:54 +0200

    Initial import

---
+++ as/access.go
@@ -0,0 +1,45 @@
+package as
+
+import (
+	"fmt"
+	"errors"
+)
+
+type Access struct {
+	User     string
+	Password string
+}
+
+type AccessFinder interface {
+	FindAccess(key string) (Access, error)
+}
+
+type Security struct {
+	Access map[string]Access
+}
+
+func (o *Security) FindAccess(key string) (ret Access, err error) {
+	var ok bool
+	ret, ok = o.Access[key]
+	if !ok {
+		err = errors.New(fmt.Sprintf("No access data found for '%v'", key))
+	}
+	return
+}
+
+func BuildAccessFinderFromFile(securityFile string) (ret AccessFinder, err error) {
+	security := &Security{}
+	ret = security
+	err = fillAccessData(security, securityFile)
+	return
+}
+
+func extractAccessKeys(keys []string) (ret map[string]Access) {
+	ret = make(map[string]Access)
+	for _, item := range keys {
+		if _, ok := ret[item]; !ok {
+			ret[item] = Access{}
+		}
+	}
+	return
+}
---
+++ as/access_console.go
@@ -0,0 +1,47 @@
+package as
+
+import (
+	"bufio"
+	"os"
+	"fmt"
+	"strings"
+	"gee/cfg"
+	"github.com/howeyc/gopass"
+)
+
+func BuildAccessFinderFromConsole(keys []string) (ret AccessFinder, err error) {
+	security := &Security{}
+	ret = security
+	security.Access = extractAccessKeys(keys)
+	fillAccessDataFromConsole(security)
+	return
+}
+
+func fillAccessDataFromConsole(security *Security) (err error) {
+	reader := bufio.NewReader(os.Stdin)
+	var text string
+	var pw []byte
+	for key, item := range security.Access {
+		fmt.Printf("Enter access data for '%v'\n", key)
+
+		fmt.Print("User: ")
+		text, err = reader.ReadString('\n')
+		if err != nil {
+			break
+		}
+		item.User = strings.TrimSpace(text)
+
+		fmt.Print("Password: ")
+		pw, err = gopass.GetPasswdMasked()
+		if err != nil {
+			break
+		}
+		item.Password = string(pw)
+		security.Access[key] = item
+	}
+	return
+}
+
+func fillAccessData(security *Security, file string) (err error) {
+	return cfg.UnmarshalFile(security, file)
+}
