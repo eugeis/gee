55de76770e27d89e5f19b7daee2ef46ed8ed2991
Eugen Eisler <eoeisler@gmail.com>
2017-06-27 18:39:46 +0200

    Example for enum

--- enum.go
+++ enum.go
@@ -1,10 +1,65 @@
 package gee
 
-type Enum interface {
-	Name() string
-	Ordinal() int
+type ComplexEnum struct {
+	name  string
+	ordinal int
+	order int
 }
 
-type EnumLiterals struct {
-	Values *[]Enum
+func (o *ComplexEnum) Name() string {
+	return o.name
 }
+
+func (o *ComplexEnum) Ordinal() int {
+	return o.ordinal
+}
+
+func (o *ComplexEnum) Order() int {
+	return o.order
+}
+
+func (o *ComplexEnum) IsLitName1() bool {
+	return o == _complexEnums.LitName1()
+}
+
+func (o *ComplexEnum) IsLitName2() bool {
+	return o == _complexEnums.LitName2()
+}
+
+type complexEnums struct {
+	values []*ComplexEnum
+}
+
+var _complexEnums = &complexEnums{values: []*ComplexEnum{
+	{name: "LitName1", ordinal: 0, order: 0},
+	{name: "LitName2", ordinal: 1, order: 1}},
+}
+
+func ComplexEnums() *complexEnums {
+	return _complexEnums
+}
+
+func (o *complexEnums) Values() []*ComplexEnum {
+	return o.values
+}
+
+func (o *complexEnums) LitName1() *ComplexEnum {
+	return _complexEnums.values[0]
+}
+
+func (o *complexEnums) LitName2() *ComplexEnum {
+	return _complexEnums.values[1]
+}
+
+func (o *complexEnums) ParseComplexEnum(name string) (ret *ComplexEnum, ok bool) {
+	switch name {
+	case "LitName1":
+		ret = o.LitName1()
+	case "LitName2":
+		ret = o.LitName2()
+	}
+	return
+}
+
+
+
--- enum_test.go
+++ enum_test.go
@@ -1,30 +1,81 @@
 package gee
 
+import (
+	"time"
+)
+
 type ComplexEnum struct {
-	name  string
-	order int
+	name    string
+	ordinal int
+	order   int
 }
 
-type ComplexEnums struct {
-	values []*ComplexEnum
+func (o *ComplexEnum) Name() string {
+	return o.name
 }
 
-var _complexEnums = &ComplexEnums{values: []*ComplexEnum{
-	{name: "", order: 0},
-}}
-
-func ComplexEnumss() *ComplexEnums {
-	return _complexEnums
+func (o *ComplexEnum) Ordinal() int {
+	return o.ordinal
 }
 
-func (o *ComplexEnums) LitName1() *ComplexEnum {
-	return o.values[0]
-}
-
-func (o *ComplexEnums) Values() []*ComplexEnum {
-	return o.values
+func (o *ComplexEnum) Order() int {
+	return o.order
 }
 
 func (o *ComplexEnum) IsLitName1() bool {
 	return o == _complexEnums.LitName1()
+}
+
+func (o *ComplexEnum) IsLitName2() bool {
+	return o == _complexEnums.LitName2()
+}
+
+type complexEnums struct {
+	values []*ComplexEnum
+}
+
+var _complexEnums = &complexEnums{values: []*ComplexEnum{
+	{name: "LitName1", order: 0},
+	{name: "LitName2", order: 1}},
+}
+
+func ComplexEnums() *complexEnums {
+	return _complexEnums
+}
+
+func (o *complexEnums) Values() []*ComplexEnum {
+	return o.values
+}
+
+func (o *complexEnums) LitName1() *ComplexEnum {
+	return _complexEnums.values[0]
+}
+
+func (o *complexEnums) LitName2() *ComplexEnum {
+	return _complexEnums.values[1]
+}
+
+func (o *complexEnums) ParseComplexEnum(name string) (ret *ComplexEnum, ok bool) {
+	switch name {
+	case "LitName1":
+		ret = o.LitName1()
+	case "LitName2":
+		ret = o.LitName2()
+	}
+	return
+}
+
+type Trace struct {
+	createdAt time.Time
+	updatedAt time.Time
+	modifiedBy string
+}
+
+func NewTrace(createdAt time.Time, updatedAt time.Time, modifiedBy string) (ret Trace, err error) {
+	ret = Trace{
+		createdAt: createdAt,
+		updatedAt: updatedAt,
+		modifiedBy: modifiedBy,
+	}
+	return
 }
